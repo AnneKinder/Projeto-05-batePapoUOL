51056b34107a6d023a255db39b3af2feb0f7b235
Annekin <annekhd98@gmail.com>
2022-04-15 15:13:07 -0300

    JS mensagens entrando no feed

--- scripts.js
+++ scripts.js
@@ -1,23 +1,51 @@
+let usuario; //GLOBAL
+let texto; //GLOBAL
+let mensagens = document.querySelector(".mensagens")  //GLOBAL //ENCONTRA DIV P/INSERIR MENSAGENS
+
 //ADQUIRIR NOME DO USUARIO
 function login(){
-let usuario = prompt("Qual o seu nome?")
+usuario = prompt("Qual o seu nome?")
 console.log(usuario)
 }
 login()
 
-//ENCONTRA DIV P/INSERIR MENSAGENS
-let feed = document.querySelector(".feed")
+//SÓ CRIA VARIAVEL TEXTO (COM O VALUE DO INPUT)
+function send(){
+    texto = document.querySelector("input").value
+    console.log(texto)
+    msgEnviada()
+    document.querySelector("input").value = ""
+
+    }
+    send()
+    
+    
 
 
 
 
+//INSERIR MSG NO FEED
 function msgEnviada(){
-    feed.innerHTML = ` <div class="text-box">
-        <div class="horario">(09:21:45) </div>
-        <div class="user-acao-destinatario bold"> ${usuario} <span class="h1">para</span> Todos: </div>
-        <div class="texto h1">Bom dia, caras!</div>
-        </div> `
-
+     mensagens.innerHTML += ` <div class="text-box">
+    <div class="horario">(09:21:45) </div>
+    <div class="user-acao-destinatario bold"> ${usuario} <span class="h1">para</span> Todos: </div>
+    <div class="texto h1"> ${texto}</div>
+    </div> `
 }
-msgEnviada()
+
+
+
+//ENCONTRA DIV P/INSERIR INPUT
+  //GLOBAL
+
+
+//INSERIR INPUT NO FEED
+//function inputar(){
+ //   let digitarTexto = document.querySelector("input").value 
+  //  console.log(digitarTexto)
+    
+
+inputar()
+
+
 
